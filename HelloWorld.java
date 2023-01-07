c84e2b4ce9df7a32765909f4ad993388517cc8b0
Abhinavdas1996 <abhinavdas3005@gmail.com>
2023-01-07 20:18:26 +0530

    print hello world

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        boolean a = true;
+        if (a == false) {
+            System.out.println("Abhinav Das");
+        } else
+            System.out.println("None..");
+    }
+}
