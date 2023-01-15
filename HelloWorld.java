6fef62fbcbe643f2a2b539cbaa7194ece1e7a0b3
Abhinavdas1996 <abhinavdas3005@gmail.com>
2023-01-15 22:30:25 +0530

    Trim

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,11 +5,14 @@ public class HelloWorld {
         System.out.println("Hello World!");
 
         String s1="Rahul Shetty Academy";
-        String[] splittedArray =  s1.split(" ");
+        String[] splittedArray =  s1.split("Shetty");
         System.out.println(splittedArray[0]);
+        System.out.println(splittedArray[1].trim());
+
+        System.out.println("**** ****** ****** *****");
 
         for (int i=0; i<splittedArray.length; i++){
-            System.out.println(splittedArray[i]);
+            System.out.println(splittedArray[i].trim());
         }
         System.out.println("* * * * * ** *********");
 
